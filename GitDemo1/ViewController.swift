7f80171eb207a356293762dc19fbdcc3eed91dc7
AndreySH <andrey649221@gmail.com>
2022-03-29 17:49:01 +0300

    initial commit

---
+++ GitDemo1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitDemo1
+//
+//  Created by Andrey on 29.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
