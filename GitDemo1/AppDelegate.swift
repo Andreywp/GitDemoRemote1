5651e1ae1792cad3a03516883a69abfce03ce164
AndreySH <andrey649221@gmail.com>
2022-03-29 19:24:56 +0300

    second commit

--- GitDemo1/AppDelegate.swift
+++ GitDemo1/AppDelegate.swift
@@ -16,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         return true
     }
+    
+    // test second commit
 
     // MARK: UISceneSession Lifecycle
 
